d7580050fc2579fd4dd3b601d89d97bab33846a5
Sitaram Dubagunta <srdk7975@gmail.com>
2025-10-16 10:15:40 +0530

    Update bstq1.c

--- bstq1.c
+++ bstq1.c
@@ -21,6 +21,72 @@ void inorderTraversal(Node * root){
 
 
 }
+void postorderTraversal(Node * root){
+    if(root == NULL)return;
+    postorderTraversal((root)->Left);
+    postorderTraversal((root)->Right);
+
+    printf("%d" ,(root)->data);
+}
+
+void preorderTraversal(Node * root){
+    if(root == NULL)return;
+
+    printf("%d" , root->data);
+
+    preorderTraversal(root->Left);
+    preorderTraversal(root->Right);
+
+
+}
+
+void PrintParent(Node * root , int targetdata){
+    if(root == NULL)return;
+
+
+    if((root->Left && root->Left->data == targetdata) || (root->Right && root->Right->data == targetdata)){
+
+
+        printf("the parent of %d is %d" , targetdata  , root->data);
+    }
+
+    PrintParent(root->Left , targetdata);
+    PrintParent(root->Right , targetdata);
+
+
+
+}
+int PrintAncestor(Node * root , int targetdata){
+
+    if(root == NULL)return 0;
+
+    if(root->data == targetdata ) return 1;
+
+    if((PrintAncestor(root->Left , targetdata) )|| (PrintAncestor(root->Right , targetdata))){
+        printf("%d ",root->data);
+
+        return 1;
+    }
+    return 0;
+
+
+
+
+
+}
+
+int DepthofTree(Node * root){
+
+    if(root == NULL)return 0;
+
+    int left = DepthofTree(root->Left);
+    int right = DepthofTree(root->Right);
+
+
+    return( left > right ? left : right )+1;
+
+
+}
 
 void  createNodes(Node ** root , int data){
 
@@ -34,8 +100,9 @@ void  createNodes(Node ** root , int data){
         return;
     }
 
-    if(data < (*root)->data){
-         createNodes(&(*root)->Left , data);
+    if(data < (*root)->data){root->Left && root->Left->data == targetdata)
+         createNodes(&(*root
+)->Left , data);
     }else{
 
         createNodes(&(*root)->Right , data);
@@ -52,7 +119,7 @@ int main(){
 
 
     Node *root = NULL;
-
+  /*
     createNodes(&root , 3);
     createNodes(&root , 1);
     createNodes(&root , 2);
@@ -62,7 +129,20 @@ int main(){
     createNodes(&root , 6);
     createNodes(&root , 11);
     createNodes(&root , 0);
+    printf("%d" , DepthofTree(root));
+    */;
+    //inorderTraversal(root);
 
-    inorderTraversal(root);
+
+    //PrintParent(root , 11);
+
+
+    createNodes(&root ,6);
+    createNodes(&root , 4);
+    createNodes(&root , 10);
+    createNodes(&root , 2);
+    createNodes(&root , 0);
+    createNodes(&root , 1);
+    PrintAncestor(root  , 2);
     return 0;
 }
